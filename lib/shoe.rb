92df1c2a982a13a317958d6188c65748d81b4f90
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-28 03:03:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -41,8 +41,8 @@ class Shoe
   end 
   
   def cobble
-    condition = @new 
-      puts "Your shoe is as good as #{@new}!"
+    @condition = "new" 
+      puts "Your shoe is as good as #{@condition}!"
   end 
   
 end 
